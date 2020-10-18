ea8b773ba3b9478ab2dc0f74a34a53d49c9586f4
Nikita Bloshchanevich <nikblos@outlook.com>
2020-10-18 16:01:19 +0200

    Add a test for mapping types

---
+++ test/rsc/expansion-tests/type/template/.ptemplate.el
@@ -0,0 +1,13 @@
+;;; .ptemplate.el --- Test `ptemplate-map' + type -*- lexical-binding: t -*-
+
+;;; Commentary:
+;; Ensure that `ptemplate-map''s type specification is respected and that
+;; ignoring root works.
+
+;;; Code:
+
+(require 'ptemplate)
+
+(ptemplate! :ignore "/" :map ("/nonsnippet.autoyas" "/snippet.autoyas" 'copy))
+
+;;; .ptemplate.el ends here
