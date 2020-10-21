ab12bb8cd6bd882f4c10292091c736ab271ab532
Nikita Bloshchanevich <nikblos@outlook.com>
2020-10-21 17:20:20 +0200

    .dir-locals.el: set projectile compile command

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ((git-commit-mode
   (git-commit-major-mode . git-commit-elisp-text-mode))
  (nil
-  (projectile-project-test-cmd "make check")))
+  (projectile-project-compilation-cmd . "make all")
+  (projectile-project-test-cmd . "make check")))
